a600b5d526b932a7c19fea6d9fdd758aeb6a9ed3
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-07-23 16:21:03 +0300

    array implementation

---
+++ data-structure/array/my_array.py
@@ -0,0 +1,35 @@
+'''Implementation of the array'''
+
+
+class MyArray:
+    ''' 
+    '''
+
+    def __init__(self):
+        self.length = 0
+        self.data = {}
+
+    def get(self, index):
+        return self.data[index]
+
+    def push(self, item):
+        self.data[self.length] = item
+        self.length += 1
+        return self.length
+
+    def pop(self):
+        last_item = self.data[self.length - 1]
+        del self.data[self.length - 1]
+        self.length -= 1
+        return last_item
+
+    def delete(self, index):
+        item = self.data[index]
+        self.shift_items(index)
+        return item
+
+    def shift_items(self, index):
+        for i in range(index, self.length - 1):
+            self.data[i] = self.data[i - 1]
+        del self.data[self.length - 1]
+        self.length -= 1
